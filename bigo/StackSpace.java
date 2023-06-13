3ed4c977b487b8c38c2a8cd51178cd4f3d04b8c6
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-13 10:41:35 -0700

    Move comment.

--- bigo/StackSpace.java
+++ bigo/StackSpace.java
@@ -2,18 +2,18 @@ package bigo;
 
 public class StackSpace {
     public static void main(String[] args) {
-        /**
-         * Each call adds a level to the stack.
-         * 
-         * sum(4) returns 10 to caller
-         *  -> sum(3) returns 6 to caller
-         *      -> sum(2) returns 3 to caller
-         *          -> sum(1) returns 1 to caller
-         *              -> sum(0) returns 0 to caller
-         */
         System.out.println(sum(4));
     }
 
+    /**
+    * Each call adds a level to the stack.
+    * 
+    * sum(4) returns 10 to caller
+    *  -> sum(3) returns 6 to caller
+    *      -> sum(2) returns 3 to caller
+    *          -> sum(1) returns 1 to caller
+    *              -> sum(0) returns 0 to caller
+    */
     static int sum(int n) {
         if (n <= 0) {
             return 0;
