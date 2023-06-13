9a34cdfbeacee70c708585dc209cb681ef4b37dd
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-13 10:40:05 -0700

    First commit

---
+++ bigo/StackSpace.java
@@ -0,0 +1,23 @@
+package bigo;
+
+public class StackSpace {
+    public static void main(String[] args) {
+        /**
+         * Each call adds a level to the stack.
+         * 
+         * sum(4) returns 10 to caller
+         *  -> sum(3) returns 6 to caller
+         *      -> sum(2) returns 3 to caller
+         *          -> sum(1) returns 1 to caller
+         *              -> sum(0) returns 0 to caller
+         */
+        System.out.println(sum(4));
+    }
+
+    static int sum(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return n + sum(n-1);
+    }
+}
