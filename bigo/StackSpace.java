2b4cc3ba82c164c6a26b8650c54e4f4fd5dbfc6b
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-13 11:00:48 -0700

    Add pairSumSequence method.

--- bigo/StackSpace.java
+++ bigo/StackSpace.java
@@ -3,6 +3,7 @@ package bigo;
 public class StackSpace {
     public static void main(String[] args) {
         System.out.println(sum(4));
+        System.out.println(pairSumSequence(4));
     }
 
     /**
@@ -22,5 +23,27 @@ public class StackSpace {
         return n + sum(n-1);
     }
 
+    /**
+     * There will be roughly O(n) calls to pairSum. However, those calls do not exist simultaneously on the call stack,
+     * so you only need O(1) space.
+     * 
+     * Adds adjacent elements between 0 and n.
+     * pairSum(0, 1) returns 1 to caller
+     * pairSum(1, 2) returns 3 to caller
+     * pairSum(2, 3) returns 5 to caller
+     * pairSum(3, 4) returns 7 to callers
+     * @param n
+     * @return
+     */
+    static int pairSumSequence(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += pairSum(i, i + 1);
+        }
+        return sum;
+    }
 
+    static int pairSum(int a, int b) {
+        return a + b;
+    }
 }
