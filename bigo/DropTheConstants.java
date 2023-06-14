8f92d3e0c28a14788f44037dd9d1715c5d4f8688
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-13 22:08:49 -0700

    Create an class to show why you drop constants.

---
+++ bigo/DropTheConstants.java
@@ -0,0 +1,45 @@
+package bigo;
+
+public class DropTheConstants {
+   public static void main(String[] args) {
+    int[] array = {1, 2, 10, 4, -5};
+    printMinMaxSingleForLoop(array);
+    printMinMaxTwoForLoops(array);
+   }
+
+   /**
+    * The time complexity of this algorithm is O(N)
+    * @param array
+    */
+   static void printMinMaxSingleForLoop(int[] array) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for (int i : array) {
+        if (i < min) min = i;
+        if (i > max) max = i;
+    }
+    printMinMax(min, max);
+   }
+
+   /**
+    * The time complexity of this algorithm is O(N).
+    * One may be tempted to say O(2N) because of the 2 (non-nested) for loops
+    * but this is incorrect.
+    * @param array
+    */
+   static void printMinMaxTwoForLoops(int[] array) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for (int i : array) {
+        if (i < min) min = i;
+    }
+    for (int i : array) {
+        if (i > max) max = i;
+    }
+    printMinMax(min, max);
+   }
+
+   private static void printMinMax(int min, int max) {
+    System.out.printf("min: %d, max: %d\n", min, max);
+   }
+}
