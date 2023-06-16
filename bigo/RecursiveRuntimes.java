6e7888e9959877ab5b11d2cd663ad618786510e0
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-16 10:47:31 -0700

    Add comment for space complexity.

--- bigo/RecursiveRuntimes.java
+++ bigo/RecursiveRuntimes.java
@@ -13,8 +13,19 @@ public class RecursiveRuntimes {
     *                        f(2)        f(2)         f(2)        f(2)
     *                      /     \      /    \        /  \        /   \   
     *                     f(1)   f(1) f(1)   f(1)   f(1) f(1)   f(1)  f(1)
-    * O(2^N - 1) = O(2^N)
+    * Time complexity O(2^N - 1) = O(2^N)
     * 
+    * f(4)
+    *   f(3)
+    *       f(2)
+    *           f(1)
+    *   f(3)
+    *       f(2)
+    *           f(1)
+    * Space complexity O(N)
+    * The algorithm starts by recursively calling the function in the return statement. Once
+    * the recursive call returns, the next set of recursive calls begin. 
+    * Therefore, the space complexity is O(2N), but we drop the constants so it's just O(N)
     * @param n
     * @return
     */
