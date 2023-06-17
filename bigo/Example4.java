57480d2832fc5d57dab22ce7237671d2bb396a01
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-16 21:39:25 -0700

    Create Example4.java

---
+++ bigo/Example4.java
@@ -0,0 +1,26 @@
+package bigo;
+
+public class Example4 {
+    public static void main(String[] args) {
+        int[] arrayA = { 12, 3, 9 };
+        int[] arrayB = { 6, 4, 2 };
+        printUnorderedPairsTwoArrays(arrayA, arrayB);
+    }
+
+    /**
+     * For each element of arrayA, the inner for loop goes through b iterations,
+     * where b = arrayB.length. If a = arrayA.length, then the runtime is O(ab)
+     * 
+     * @param arrayA
+     * @param arrayB
+     */
+    static void printUnorderedPairsTwoArrays(int[] arrayA, int[] arrayB) {
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i] < arrayB[j]) {
+                    System.out.println(arrayA[i] + ", " + arrayB[j]);
+                }
+            }
+        }
+    }
+}
