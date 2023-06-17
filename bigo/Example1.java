15d8a10d87677e100cea2c4856223ed6e9e9bd21
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-16 21:05:31 -0700

    Fix formatting.

--- bigo/Example1.java
+++ bigo/Example1.java
@@ -2,23 +2,25 @@ package bigo;
 
 public class Example1 {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+        int[] array = { 1, 2, 3, 4, 5 };
         printSumAndProduct(array);
     }
 
     /**
-     * The time complexity is O(N). The fact that we iterate through the array twice does not matter.
+     * The time complexity is O(N). The fact that we iterate through the array twice
+     * does not matter.
+     * 
      * @param array
      */
     static void printSumAndProduct(int[] array) {
         int sum = 0;
         int product = 1;
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         for (int i = 0; i < array.length; i++) {
             product *= array[i];
         }
-        System.out.println(sum + ", "+ product);
+        System.out.println(sum + ", " + product);
     }
 }
