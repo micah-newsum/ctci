e66f2a0e5cedc27e947f3ed1b03b8795de2b7210
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-19 21:40:49 -0700

    Add space complexity documentation.

--- bigo/RecursiveRuntimes.java
+++ bigo/RecursiveRuntimes.java
@@ -5,34 +5,33 @@ public class RecursiveRuntimes {
     System.out.println(f(4));
    }
 
-   /**
-    *                                           f(4)
-    *                               /                      \
-    *                              f(3)                    f(3)
-    *                          /         \             /         \
-    *                        f(2)        f(2)         f(2)        f(2)
-    *                      /     \      /    \        /  \        /   \   
-    *                     f(1)   f(1) f(1)   f(1)   f(1) f(1)   f(1)  f(1)
-    * Time complexity O(2^N - 1) = O(2^N)
-    * 
-    * f(4)
-    *   f(3)
-    *       f(2)
-    *           f(1)
-    *   f(3)
-    *       f(2)
-    *           f(1)
-    * Space complexity O(N)
-    * The algorithm starts by recursively calling the function in the return statement. Once
-    * the recursive call returns, the next set of recursive calls begin. 
-    * Therefore, the space complexity is O(2N), but we drop the constants so it's just O(N)
-    * @param n
-    * @return
-    */
-   static int f(int n) {
-    if (n <= 1) {
-        return 1;
+    /**
+     * 
+     * Time complexity: O(2^N - 1) = O(2^N)
+     *                                           f(4)
+     *                               /                      \
+     *                              f(3)                    f(3)
+     *                          /         \             /         \
+     *                        f(2)        f(2)         f(2)        f(2)
+     *                      /     \      /    \        /  \        /   \   
+     *                     f(1)   f(1) f(1)   f(1)   f(1) f(1)   f(1)  f(1)
+     * 
+     * Space complexity: O(N) 
+     *                                f(4)
+     *               f(3)                               f(3)
+     *       f(2)            f(2)               f(2)            f(2)
+     *   f(1)    f(1)    f(1)    f(1)       f(1)    f(1)    f(1)    f(1)
+     * 
+     * The space complexity is O(N) because there will be at most N functions on the call stack.
+     * The easiest way to recognize this is to visualize all function calls as a binary tree, with the 
+     * left half of the tree running to completion and then the right.
+     * @param n
+     * @return
+     */
+    static int f(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return f(n - 1) + f(n - 1);
     }
-    return f(n - 1) + f(n - 1);
-   }
 }
