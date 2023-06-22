f3542eadf2c35fd44de97606fa9dd3a30f6a25a0
Micah Newsum <micah.newsum@glidewelldental.com>
2023-06-22 13:59:53 -0700

    Update Example12.java documentation.

--- bigo/Example12.java
+++ bigo/Example12.java
@@ -13,37 +13,52 @@ public class Example12 {
      * 
      * Pseudo code:
      * permutation("car", "")
-     *      for i to "car".length:
+     *      for i to i < "car".length:
      *          i = 0
      *          permutation("ar", "c")
-     *              for i to "ar".length:
+     *              for i to i < "ar".length:
      *                  i = 0
      *                  permutation("r", "ca")
      *                      for i to "r".length:
      *                          i = 0
      *                          permutation("", "car")
-     *                              print "car" <----
+     *                              print "car" 
      *                  i = 1
      *                  permutation("a", "cr")
-     *                      for i to "a".length:
+     *                      for i to i < "a".length:
      *                          i = 0
      *                          permutation("", "cra")
-     *                              print "cra" <----
+     *                              print "cra" 
      *          i = 1
      *          permutation("cr", "a")
-     *              for i; i < "cr".length:
+     *              for i to i < "cr".length:
      *                  i = 0
      *                  permutation("r", "ac")
      *                      for i; i < "r".length:
      *                          i = 0
      *                          permutation("", "acr")
-     *                              print "acr" <----
+     *                              print "acr" 
+     *                  i = 1
+     *                  permutation("c", "ar")
+     *                      for i to i < "c".length:
+     *                          i = 0
+     *                          permutation("", "arc")
+     *                              print "arc" 
      *          i = 2
-     *                          
-     *                                         
-     * 
-     * 
-     *      
+     *          permutation("ca", "r")
+     *              for i < "ca".length:
+     *                  i = 0
+     *                  permutation("a", "rc")
+     *                      for i < "a".length:
+     *                          i = 0
+     *                          permutation("", "rca")
+     *                              print "rca"
+     *                  i = 1
+     *                  permutation("c", "ra")
+     *                      for i < "c".length:
+     *                          i = 0
+     *                          permutation("", "rac")
+     *                              print "rac"
      * @param str
      * @param prefix
      */
