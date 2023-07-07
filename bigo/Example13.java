e59d748287f5bf69992a815502b934baccb08fc4
Micah Newsum <micah.newsum@glidewelldental.com>
2023-07-06 21:37:27 -0700

    Create Example13 and accompanying text.

---
+++ bigo/Example13.java
@@ -0,0 +1,16 @@
+package bigo;
+
+public class Example13 {
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+
+    /*
+     * Computes the Nth Fibonacci number 
+     */
+    static int fib(int n) {
+        if (n <= 0) return 0;
+        else if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
