ce45ec05650a5960f90189fe2e09f57c400839a0
Micah Newsum <micah.newsum@glidewelldental.com>
2025-10-28 08:35:58 -0700

    feat: implemented V1.10

--- bigo/additionalproblems/VI_10.java
+++ bigo/additionalproblems/VI_10.java
@@ -1,4 +1,19 @@
 package additionalproblems;
 
 public class VI_10 {
+    public static void main(String[] args) {
+       System.out.println("Ths sum of the digits in 1 is: "+sumDigits(1));
+       System.out.println("Ths sum of the digits in 12 is: "+sumDigits(12));
+       System.out.println("Ths sum of the digits in 123 is: "+sumDigits(123));
+       System.out.println("Ths sum of the digits in 1234 is: "+sumDigits(1234));
+    }
+
+    private static int sumDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
 }
