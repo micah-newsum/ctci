436cdd621b3a821dc39a5c7e23e99855c73ac881
Micah Newsum <micah.newsum@glidewelldental.com>
2026-08-25 12:50:31 -0700

    refactor: remove redundant access modifiers

--- src/main/java/chapter3/Stack.java
+++ src/main/java/chapter3/Stack.java
@@ -1,8 +1,8 @@
 package chapter3;
 
 public interface Stack<T> {
-    public void push(T item);
-    public T pop();
-    public boolean isEmpty();
-    public int size();
+    void push(T item);
+    T pop();
+    boolean isEmpty();
+    int size();
 }
